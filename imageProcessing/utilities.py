55d9bf5cf1ea50073bf95a73ae115a8003acab59
Juan Carlos <jucar251096@gmail.com>
2021-06-21 17:09:43 -0500

    First commit.

---
+++ imageProcessing/utilities.py
@@ -0,0 +1,8 @@
+
+class Video:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def test_class():
+        print("Class Working")
---
+++ main.py
@@ -0,0 +1,8 @@
+# Main script for test
+from imageProcessing.utilities import Video
+
+
+if __name__ == '__main__':
+    print('Starting ...')
+    Video.test_class()
+
