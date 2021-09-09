b8146ce065b8f8179be240cec2939182028ae5ca
Juan Carlos <jucar251096@gmail.com>
2021-09-08 19:32:40 -0500

    FramesSended created.

--- imageProcessing/utilities.py
+++ imageProcessing/utilities.py
@@ -1,9 +1,15 @@
 import torch
 import cv2
 import os
+import boto3
+import datetime
 
 
 class VideoCaptured:
+    """'
+       Class that can be used to detect real time objects using yolo5.
+       Use play_video or stream_video to start operations.
+    """
     def __init__(self, cap, testname, modelType, stream=False, pad=50):
         self.model = torch.hub.load('ultralytics/yolov5',
                                     modelType,
@@ -17,7 +23,7 @@ class VideoCaptured:
         if not self.stream:
             self.frames = self.get_all_frames()
 
-    def model_conf(self, conf=0.5, iou=0.45, classes=[0]):
+    def model_conf(self, conf=0.5, iou=0.45, classes=None):
         self.model.conf = conf
         self.model.iou = iou
         self.model.classes = classes
@@ -154,3 +160,107 @@ class VideoCaptured:
                 break
             print(count)
         cv2.destroyAllWindows()
+
+
+class FramesSended:
+    """"
+        Class that uses yolov5 toi detect specific objects and send frames to S3 buckets.
+    """
+    def __init__(self, cap, rbpiID, modelType, bucketName):
+        """"
+            Inputs:
+                Cap: capture object from opencv, it have to be a stream video
+                sessionName: Name of the session
+                modelType: Model used in yolov5. i.e. = yolov5s, yolov5l
+        """
+        self.model = torch.hub.load('ultralytics/yolov5',
+                                    modelType,
+                                    pretrained=True)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model_conf()
+        self.cap = cap
+        self.rbpiID = rbpiID
+        self.bucketName = bucketName
+        # Creating the high level object oriented interface
+        self.resource = boto3.resource(
+            's3',
+            aws_access_key_id='AKIAZC5KVF2CCPQ342Z4',
+            aws_secret_access_key='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',
+            region_name='us-west-2'
+        )
+
+    def model_conf(self, conf=0.5, iou=0.5, classes=None):
+        """"
+            Configuration of the yolo5 model. Ref: https://github.com/ultralytics/yolov5/issues/36
+            model.conf = confidence threshold (0-1), default 0.5
+            model.iou = NMS IoU threshold (0-1), default, 0.5
+            model.classes = (optional list) filter by class, i.e. = [0, 15, 16] for persons, cats and dogs
+        """
+        self.model.conf = conf
+        self.model.iou = iou
+        self.model.classes = classes
+        self.model.to(self.device)
+
+    def score_frame(self, frame):
+        """"
+            Score frame and detect objects.
+            Inputs
+                frame: frame to be analized
+            Outputs
+                labels: class number code
+                labels_string: string of the class
+                cords: cordenates of the square where the object is
+        """
+        results = self.model(frame, size=640)
+        # labels = results.xyxyn[0][:, -1].numpy()
+        labels_string = results.pandas().xyxyn[0]['name'].to_list()
+        # cords = results.xyxyn[0][:, :-1].numpy()
+        return labels_string
+
+    def start_process(self):
+        """"
+            Starts sending images detected to a bukets s3.
+        """
+        if not self.cap.isOpened():
+            print("Cannot open camera")
+            return None
+        while True:
+            # Capture frame-by-frame
+            datetime.datetime.now()
+            # frameRate = self.cap.get(5)  # frame rate
+            ret, frame = self.cap.read()
+            frameId = self.cap.get(1)  # current frame number
+            # print(frameId)
+            # print(ret)
+            # print('-----')
+
+            # if frame is read correctly ret is True
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+
+            # Our operations on the frame come
+            ###########################################
+            date = datetime.datetime.now()
+            image_string = cv2.imencode('.jpg', frame)[1].tostring()
+            labels = self.score_frame(frame)
+            # print(labels)
+            if labels:
+                fileName = date.strftime('%Y%m%d') + date.strftime('_%H%M%S_') + str(labels[0]) \
+                           + '_' + str(frameId) + '.jpg'
+                # print(fileName)
+                destinationPath = str(self.rbpiID) + '/' + date.strftime('%Y') + '/' + date.strftime('%m') + \
+                                  '/' + date.strftime('%d') + '/' + fileName
+                # print(destinationPath)
+                self.resource.Bucket(self.bucketName).put_object(Key=destinationPath,
+                                                                 Body=image_string)
+                # print('hay una imagen')
+            ############################################
+            # cv2.imshow('frame', frame)
+            # if cv2.waitKey(1) == ord('q'):
+            #     break
+
+        # When everything done, release the capture
+        self.cap.release()
+        cv2.destroyAllWindows()
+
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # Main script for test
 import cv2
-import boto3
-import torch
-from imageProcessing.utilities import VideoCaptured
-import numpy as np
+from imageProcessing.utilities import VideoCaptured, FramesSended
+
 '''
 set AWS_SECRET_ACCESS_KEY=SvinHqNhKFxcTAhLe6wsNzqHjQ6zngiuWCYamH5h
 set AWS_ACCESS_KEY_ID=AKIAZC5KVF2CCPQ342Z4
@@ -15,19 +13,17 @@ if __name__ == '__main__':
 
     # variables
     pad = 50
+    rbpiID = 'rbpi_0'
+    bucketName = 'framesfromcameras'
 
     # Model configuration
-    modelType = 'yolov5s'
+    modelType = 'yolov5s6'
     # Video
     # cap = cv2.VideoCapture("rtsp://192.168.1.109:554/11")
     # cap = cv2.VideoCapture("rtsp://admin:123@169.254.249.184/ch0_0.264")
 
     cap = cv2.VideoCapture(0)
-    # cap = cv2.VideoCapture(endpoint)
-    # cap = cv2.VideoCapture(2)
-    video = VideoCaptured(cap, 'testCamera', modelType, stream=True)
-    # video.model_conf(classes=[[2, 3, 5, 7]])
-    video.stream_video(recognize=True, mode='draw')
-    # video.play_video(recognize=True, mode='cut', save=True)
 
+    video = FramesSended(cap, rbpiID, modelType, bucketName)
 
+    video.start_process()
